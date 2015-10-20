58a2fc07d9cf2e2d14505e53ef2d219a5ca75be4
iwao1752 <101.4.job@gmail.com>
2015-10-20 14:35:35 +0900

    changed method add argument statement

--- dokoJava/src/dokoJava/List5.java
+++ dokoJava/src/dokoJava/List5.java
@@ -1,16 +1,17 @@
 package dokoJava;
 
 public class List5 {
-	public static void methodA(){
-		System.out.println( "methodA");
-		methodB();
-	}
-	
-	public static void methodB(){
-		System.out.println("methodB");
-	}
-	
 	public static void main(String[] args){
-		methodA();
+		System.out.println("----call method----");
+		
+		hello("minato");
+		hello("asaka");
+		hello("sugawara");
+		
+		System.out.println("----finished calling method----");
+		}
+	
+	public static void hello(String name){
+		System.out.println( "hello, " + name );
 	}
 }
