94884f91bf99ab67a9f250806811b5d19fa13712
iwao1752 <101.4.job@gmail.com>
2015-10-22 11:25:42 +0900

    create test4

---
+++ dokoJava/src/dokoJava/Test4_3.java
@@ -0,0 +1,10 @@
+package dokoJava;
+
+public class Test4_3 {
+	public static void main(String[] args){
+		int[] counts = {0,1,2,3};
+		float[] heights = {171.3F, 175.0F};
+		System.out.println(counts[1]);
+		System.out.println(heights[1]);
+	}
+}
